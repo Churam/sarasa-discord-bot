57880ee9ebe43cb001f3501a973b127e9e337e00
Solan Delvenne <sdelvenn@e-r4-p2.s19.be>
2019-02-04 14:15:27 +0100

    Started migrating to SQLite3 from JSON

--- bot/hanapara.py
+++ bot/hanapara.py
@@ -11,6 +11,18 @@ import requests
 from io import BytesIO
 from datetime import datetime
 import asyncio
+import sqlite3
+
+
+#db connection
+dbcon = sqlite3.connect("./database/sarasa_db.sqlite3")
+
+curs = dbcon.cursor()
+
+async def search_userdata(pk, col_name, table = "main"):
+	infos = [col_name, table, int(pk)]
+	res = curs.execute("SELECT (?) FROM (?) WHERE uid = (?)", infos)
+	print(res)
 
 
 #Server id for the Contributioncheck command
